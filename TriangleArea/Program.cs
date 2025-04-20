93a3f1136fdc1e584c32fcc6a5c8a382d58f7918
Путинцев Дмитрий <putdv@mail.ru>
2025-04-20 14:51:01 +0300

    Добавьте файлы проекта.

---
+++ TriangleArea/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace TriangleSquare
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Посчитаем площадь равностороннего треугольника. Для этого введите, пожалуйста, длину периметра");
+            double p = double.Parse(Console.ReadLine());
+            Console.WriteLine("Сторона                       Площадь");
+            double storona = p/3;
+            double pp = p/2;
+            double square = Math.Sqrt(pp*(pp-storona)* (pp - storona)* (pp - storona));
+            Console.WriteLine($"{storona}      {square}");
+        }
+    }
+}
